0c518dc26db901fc3c928b1233f1f8daeedbfbd8
yiningle <a1787954119@gmail.com>
2024-03-22 17:49:49 +0800

    Making code Clearer with Iterator Adaptors

--- src/lib.rs
+++ src/lib.rs
@@ -52,15 +52,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    // let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(&query) {
-            results.push(line);
-        }
-    }
+    // for line in contents.lines() {
+    //     if line.contains(&query) {
+    //         results.push(line);
+    //     }
+    // }
 
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+
+    // results
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
